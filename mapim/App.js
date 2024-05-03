a860b6cdd06a3255b5503679e1f849385012382d
Miguel <castilhomiguel01@gmail.com>
2024-05-03 14:32:58 -0300

    Só pra aprender a comittar

--- mapim/App.js
+++ mapim/App.js
@@ -11,7 +11,7 @@ export default function App() {
     <View style={styles.maincontainer}>
 
 
-      <Image source={require("./assets/rural_logosplash.png")}
+      <Image source={require("./assets/rural_logosplash.png")}  //Miguel Esteves Acá
         style={styles.backgroundimage}
       />
 
