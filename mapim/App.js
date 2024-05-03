fe6f06b00b229446bb24a62ce2ae6ea00bbede03
gusropdev <gusropdev@hotmail.com>
2024-05-03 13:37:56 -0300

    Tem que arrumar a porra do botão que sobe qnd abre o teclado e tá feio qnd clica

---
+++ mapim/App.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons } from 'react-native-vector-icons'
+
+export default function App() {
+
+  const [text, setText] = useState("");
+
+  return (
+    <View style={styles.maincontainer}>
+
+      <View style={{ alignItems: 'center' }}>
+        <Image source={require("./assets/rural_logo03.png")}
+          style={styles.backgroundimage}
+        />
+
+        <StatusBar style="auto" />
+        <Image
+          source={require("./assets/rural_logo03.png")}
+          style={styles.mainlogo} />
+
+        <View style={[styles.searchcontainer, styles.boxshadow]}>
+          <TextInput
+            style={{ justifyContent: 'flex-end', flex: 9 }}
+            placeholder='Sala, professor, bloco...'
+            value={text}
+            onChangeText={(value) => setText(value)}
+          />
+
+          <Ionicons
+            name='search'
+            size={30}
+            color="#284b8c"
+          />
+        </View>
+      </View>
+
+      <View style={[styles.boxshadow, { borderRadius: 30 }]}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>
+            Navegar pelo mapa
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  maincontainer: {
+    height: "100%",
+    backgroundColor: '#F5F5F5',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+
+  mainlogo: {
+    height: 180,
+    width: 180,
+    resizeMode: 'contain',
+    marginBottom: "20%",
+    marginTop: '10%'
+  },
+
+  backgroundimage: {
+    height: '125%',
+    width: '125%',
+    position: 'absolute',
+    top: 20,
+    opacity: 0.03
+  },
+
+  searchcontainer: {
+    flexDirection: 'row',
+    width: '75%',
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+    paddingRight: 16,
+    paddingLeft: 12,
+    padding: 4,
+    borderRadius: 8,
+  },
+
+  button: {
+    backgroundColor: '#284b8c',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 200,
+    paddingLeft: 15,
+    paddingRight: 15,
+    padding: 15,
+    borderRadius: 30,
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+
+  buttonText: {
+    color: 'white',
+    fontSize: 18
+  },
+
+  boxshadow: {
+    elevation: 5,
+    shadowColor: 'black'
+  }
+
+});
